897a9813649052ff61a7c30087dc16dd1d1c48f1
Akshit Tyagi <tyagiakshit833@gmail.com>
2024-01-09 22:02:28 +0530

    Add main.tf, the api_key variable and Daft Punk

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.2.6"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artist = "Daft Punk"
+  #  album = "Random Access Memories (10th Anniversary Edition)"
+  #  name  = "Give Life Back to Music"
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "The CodeJam Playlist"
+  description = "Wishing you make the nicest, most Randomly Accessible Memories this CodeR̶A̶M̶Jam :)"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
